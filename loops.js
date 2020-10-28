3168223cc0dc8a67127d83b62c19529839e84004
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-28 03:25:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,12 +20,12 @@ function whileLoop(n) {
 
 function doWhileLoop(num){
   let num = 0;
- 
+
 function incrementVariable() {
   num = num + 1;
   return num;
 }
- 
+
 do {
   console.log("I run once regardless.");
 } while (incrementVariable() < 5);
